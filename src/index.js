fc15ccb0aa0fe806bf79f814083de08fd1051e54
tunggulalmajid <tunggulmajid@gmail.com>
2026-04-22 15:02:31 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,23 +2,34 @@ const express = require("express");
 require("dotenv").config();
 const path = require("path");
 const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
 const logRequest = require("./middleware/log");
-
-const swaggerDocument = require(path.join(__dirname, "../swaggerConfig.json"));
 
 const app = express();
 
+const swaggerCssPath = path.join(
+  __dirname,
+  "../node_modules/swagger-ui-dist/swagger-ui.css",
+);
+let swaggerCss = "";
+
+try {
+  swaggerCss = fs.readFileSync(swaggerCssPath, "utf8");
+} catch (error) {
+  console.log("Gagal memuat CSS lokal, Swagger mungkin tanpa styling");
+}
+
+const swaggerDocument = require(path.join(__dirname, "../swaggerConfig.json"));
+
 app.use(express.json());
 app.use(logRequest);
-
-app.use("/public", express.static(path.join(__dirname, "../public")));
 
 app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocument, {
-    customCssUrl: "/public/swagger-ui.css",
-    customJs: "/public/swagger-ui-bundle.js",
+    customCss: swaggerCss,
+    customSiteTitle: "API Sistem Bimbel PAA",
   }),
 );
 
@@ -27,13 +38,9 @@ app.use("/api/users", require("./routes/Users"));
 app.use("/api/kelas", require("./routes/kelas"));
 
 const PORT = process.env.PORT || 3000;
-
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
-    console.log(`================================================`);
-    console.log(`Server jalan di http://localhost:${PORT}`);
-    console.log(`Swagger: http://localhost:${PORT}/api-docs`);
-    console.log(`================================================`);
+    console.log(`Server jalan di http://localhost:${PORT}/api-docs`);
   });
 }
 
